d2b044ba7ed67e4b886b14bb0eb5fe0944c569c9
Jsus_Alv <alvarezjesus279@gmail.com>
2024-06-26 12:06:23 -0400

    first commit

---
+++ Asimetrico/Asimetrico.cs
@@ -0,0 +1,51 @@
+﻿// Cifrado Asimétrico (Metodo: Rivest-Shamir-Adleman)
+
+using System.Security.Cryptography;
+using System.Text;
+
+public class Asimetrico
+{
+    public static (string, string) ClavesRSA()
+    {
+        using (RSA rsa = RSA.Create())
+        {
+            // True = Clave Privada, False = Clave Pública
+            return (rsa.ToXmlString(true), rsa.ToXmlString(false));
+        }
+    }
+
+    // Encrypt
+    public static byte[] Encrypt(string mensaje, string clavePublica)
+    {
+        using (RSA rsa = RSA.Create())
+        {
+            rsa.FromXmlString(clavePublica);
+            return rsa.Encrypt(Encoding.UTF8.GetBytes(mensaje), RSAEncryptionPadding.OaepSHA256);
+        }
+    }
+
+    // Decrypt
+    public static string Decrypt(byte[] cifrado, string clavePrivada)
+    {
+        using (RSA rsa = RSA.Create())
+        {
+            rsa.FromXmlString(clavePrivada);
+            return Encoding.UTF8.GetString(rsa.Decrypt(cifrado, RSAEncryptionPadding.OaepSHA256));
+        }
+    }
+
+    public static void Main()
+    {
+        var (clavePrivada, clavePublica) = ClavesRSA();
+        string mensaje = "Actividad 5 de la clase de criptografia";
+
+        byte[] encrypted = Encrypt(mensaje, clavePublica);
+        string decrypted = Decrypt(encrypted, clavePrivada);
+
+        // Se convierte a Base64 para poder ser leido por el usuario
+        string encryptedBase64 = Convert.ToBase64String(encrypted);
+
+        Console.WriteLine($"Mensaje Encrypted (Base64): \n\n{encryptedBase64}");
+        Console.WriteLine($"\nMensaje Decrypted: {decrypted}");
+    }
+}
---
+++ Simetrico/Simetrico.cs
@@ -0,0 +1,77 @@
+﻿// Cifrado Simétrico (Metodo: Advanced Encryption Standard)
+
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+public class Simetrico
+{
+    public static (byte[], byte[]) ClaveAESyIV()
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.GenerateKey();
+            aes.GenerateIV();
+            return (aes.Key, aes.IV);
+        }
+    }
+    
+    // Encrypt
+    public static byte[] Encrypt(string mensaje, byte[] clave, byte[] iv)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = clave;
+            aes.IV = iv;
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter sw = new StreamWriter(cs))
+                    {
+                        sw.Write(mensaje);
+                    }
+                }
+                return ms.ToArray();
+            }
+        }
+    }
+
+    // Decrypt 
+    public static string Decrypt(byte[] cifrado, byte[] clave, byte[] iv)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = clave;
+            aes.IV = iv;
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+            using (MemoryStream ms = new MemoryStream(cifrado))
+            {
+                using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader sr = new StreamReader(cs))
+                    {
+                        return sr.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+
+    public static void Main()
+    {
+        var (clave, iv) = ClaveAESyIV();
+        string mensaje = "Actividad 5 de la clase de criptografia";
+        
+        byte[] encrypted = Encrypt(mensaje, clave, iv);
+        string decrypted = Decrypt(encrypted, clave, iv);
+        
+        // Se convierte a Base64 para poder ser leido por el usuario
+        string encryptedBase64 = Convert.ToBase64String(encrypted);
+
+        Console.WriteLine($"Mensaje Encrypted (Base64):\n\n{encryptedBase64}");
+        Console.WriteLine($"\nMensaje Decrypted:\n{decrypted}");
+    }
+}
